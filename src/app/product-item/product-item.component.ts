eed5ae4db8ba07df5033d8ae9603f71882475dd5
marmai-jma <johane.marmai@gmail.com>
2019-12-19 09:06:28 +0100

    Avec un router qui marche

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../models/product';
 
+
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.css']
+  styleUrls: []
 })
 export class ProductItemComponent implements OnInit {
   @Input() product: Product;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+ngOnInit() {
+
+}
 
 }
