9cb45a7338136cfdd34a47fd147bff08b5b46dec
marmai-jma <johane.marmai@gmail.com>
2019-12-19 10:23:34 +0100

    mapping des types entre front et back dans const

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -6,29 +6,52 @@ enum ProductType {
 export class Product {
     id: number;
     name: string;
-    type: string;
+    type: ProductType;
     description: string;
     stock: number;
     photo: string;
     price: number;
+    tags: any[];
+    nutritionalInformations: any[];
+    slug: string;
+
 
     constructor(options:{
         id: number;
         name: string;
-        type: string; // metttre ici le mapping du type
+        type: string;
         description: string;
         stock: number;
         photo: string;
         price: number;
+        tags: any[];
+        nutritionalInformations: any[];
+        slug: string;
     }){
         // Object.assign(this, options); <= tout initialiser d'un coup mais sale
         this.id = options.id;
         this.name = options.name;
-        this.type = options.type;
+        // this.type = options.type;
+        switch (options.type){
+            case 'STARTER' :
+                this.type = ProductType.Entree;
+                break;
+            case 'MAIN_COURSE':
+                this.type = ProductType.Plat;
+                break;
+            case 'DESSERT':
+                this.type = ProductType.Dessert;
+                break;
+            default: this.type = ProductType.Plat;
+        }
+
         this.description = options.description;
         this.stock = options.stock;
         this.photo = options.photo;
         this.price = options.price;
+        this.tags = options.tags;
+        this.nutritionalInformations = options.nutritionalInformations;
+        this.slug = options.slug;
     }
 }
 
